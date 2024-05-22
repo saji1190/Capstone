ced71f29b07e7c6e4529cd3edc934a0cbb4348ca
Saji Thomas <168119530+saji1190@users.noreply.github.com>
2024-05-22 17:45:52 +0200

    Update vpc.tf
    adding public subnet

--- vpc.tf
+++ vpc.tf
@@ -8,3 +8,13 @@ resource "aws_vpc" "dev_vpc" {
     name     = "deham14"
   }       
 }
+resource "aws_subnet" "public-1" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "deham9"
+  }
+}
