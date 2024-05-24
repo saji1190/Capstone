26313a87ed36d17e832c0020a171c7609c4a3a32
saji1190 <sajithomas1190@gmail.com>
2024-05-24 15:53:57 +0200

    Instance creation

---
+++ ec2.tf
@@ -0,0 +1,54 @@
+locals {
+  # The name of the EC2 instance
+  name = "awsrestartproject"
+  owner = "Saji"
+}
+
+### Select the newest AMI
+
+data "aws_ami" "latest_linux_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*x86_64"]
+  }
+}
+
+### Create an EC2 instance
+
+resource "aws_instance" "instance" {
+  ami                         = data.aws_ami.latest_linux_ami.id
+  
+  instance_type               = "t2.micro"
+  availability_zone           = "us-west-2a"
+  associate_public_ip_address = true
+  key_name                    = "vockey"
+  #vpc_security_group_ids      = [aws_security_group.sg_vpc.id]
+  subnet_id                   = aws_subnet.public-1.id
+  #iam_instance_profile        = "deham14_ec2"
+  count = 1
+  tags = {
+    Name = "Deham14"
+  }
+  #user_data = file("userdata.sh")
+  #user_data = "${base64encode(data.template_file.ec2userdatatemplate.rendered)}"
+
+  provisioner "local-exec" {
+    command = "echo Instance Type = ${self.instance_type}, Instance ID = ${self.id}, Public IP = ${self.public_ip}, AMI ID = ${self.ami} >> metadata"
+  }
+} 
+
+
+#data "template_file" "ec2userdatatemplate" {
+  #template = "${file("userdata.tpl")}"
+#}
+
+#output "ec2rendered" {
+  #value = "${data.template_file.ec2userdatatemplate.rendered}"
+#}
+
+output "public_ip" {
+  value = aws_instance.instance[0].public_ip
+} 
