2a18acd652394337cc7d6070e0e803bfe50b2ee9
saji1190 <sajithomas1190@gmail.com>
2024-05-24 15:50:52 +0200

    Ifrastructure creation

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "dev_vpc" {
   enable_dns_support = true
   
   tags       =  {
-    name     = "deham14"
+    Name     = "deham14_VPC"
   }       
 }
 resource "aws_subnet" "public-1" {
@@ -15,6 +15,87 @@ resource "aws_subnet" "public-1" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "deham14"
+    Name = "deham14_publicsub1"
   }
+}
+
+
+resource "aws_subnet" "private-1" {
+  vpc_id     = aws_vpc.dev_vpc.id 
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "deham14_privatesub1" 
+  }
+}
+
+resource "aws_subnet" "public-2" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "deham14_publicsub2"
+  }
+}
+
+resource "aws_subnet" "private-2" {
+  vpc_id     = aws_vpc.dev_vpc.id 
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "deham14_privatesub2" 
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.dev_vpc.id
+  tags = {
+    Name = "deham14_IGW"
+  }
+}
+resource "aws_route_table" "RB_Public_RouteTable" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "deham14_publicRT"
+  }
+}
+
+resource "aws_route_table" "RB_Private_RouteTable" {
+  vpc_id = aws_vpc.dev_vpc.id
+  tags = {
+    Name = "deham14_privateRT"
+  }
+}
+resource "aws_route_table_association" "Public_Subnet1_Asso" {
+  route_table_id = aws_route_table.RB_Public_RouteTable.id
+  subnet_id      = aws_subnet.public-1.id
+  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-1]
+}
+
+resource "aws_route_table_association" "Private_Subnet1_Asso" {
+  route_table_id = aws_route_table.RB_Private_RouteTable.id
+  subnet_id      = aws_subnet.private-1.id
+  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-1]
+}
+
+resource "aws_route_table_association" "Public_Subnet2_Asso" {
+  route_table_id = aws_route_table.RB_Public_RouteTable.id
+  subnet_id      = aws_subnet.public-2.id
+  depends_on     = [aws_route_table.RB_Public_RouteTable, aws_subnet.public-2]
+}
+
+resource "aws_route_table_association" "Private_Subnet2_Asso" {
+  route_table_id = aws_route_table.RB_Private_RouteTable.id
+  subnet_id      = aws_subnet.private-2.id
+  depends_on     = [aws_route_table.RB_Private_RouteTable, aws_subnet.private-2]
 }
