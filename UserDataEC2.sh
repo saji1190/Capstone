ed4b4f1cc00febf08b1719751d37910eb3248fa9
saji1190 <sajithomas1190@gmail.com>
2024-07-20 12:23:50 +0200

    Adding entry for downloading and restoring the backup files s3 in another AWS account

--- UserDataEC2.sh
+++ UserDataEC2.sh
@@ -1,11 +1,12 @@
-#! /bin/bash
+#! /bin/bash -eux
 # # Install updates
 sudo yum update -y
 
 # Configure AWS CLI with IAM role credentials
 aws configure set default.region us-west-2
 
-
+#Enable EPEL Repository and Install stress-ng
+sudo amazon-linux-extras install epel
 sudo yum install -y stress-ng
 
 #Install httpd
@@ -21,7 +22,8 @@ sudo systemctl enable mariadb
 
 
 #Install PHP
-sudo yum install -y php 
+#sudo yum install -y php
+sudo amazon-linux-extras enable php7.4
 sudo amazon-linux-extras install 
 
 
@@ -95,6 +97,69 @@ sudo yum clean metadata
 sudo yum install -y php-cli php-pdo php-fpm php-json php-mysqlnd
 
 # Restart Apache
+sudo systemctl restart httpd
+sudo systemctl start mariadb
+
+
+# Create credentials and config files of sandbox and set ownership and permissions
+sudo mkdir /home/ec2-user/.aws
+sudo touch /home/ec2-user/.aws/credentials /home/ec2-user/.aws/config
+sudo chmod 600 /home/ec2-user/.aws/credentials /home/ec2-user/.aws/config
+sudo chmod 755 /home/ec2-user/.aws
+sudo chown ec2-user:ec2-user /home/ec2-user/.aws -R
+
+# Configure AWS credentials
+sudo echo "[default]" > /home/ec2-user/.aws/credentials
+sudo echo "aws_access_key_id=${access_key}" >> /home/ec2-user/.aws/credentials
+sudo echo "aws_secret_access_key=${secret_key}" >> /home/ec2-user/.aws/credentials
+sudo echo "aws_session_token=${session_token}" >> /home/ec2-user/.aws/credentials
+
+# Configure AWS config
+sudo echo "[default]" > /home/ec2-user/.aws/config
+sudo echo "output = json" >> /home/ec2-user/.aws/config
+sudo echo "region = ${region}" >> /home/ec2-user/.aws/config
+
+# # Variables
+# S3_BUCKET="saji-worpress24"
+# HTML_FILE="html.zip"
+# DB_FILE="dbresume90_14-24.sql"
+# DOWNLOAD_DIR="/tmp"
+# WEB_DIR="/var/www"
+
+# Step 1: Download files from S3
+#echo "Downloading $HTML_FILE from S3..."
+sudo -u ec2-user aws s3 sync s3://saji-worpress24/ /var/www
+
+# Step 2: Extract HTML files to /var/www
+#echo "Extracting $HTML_FILE to $WEB_DIR..."
+sudo mv /var/www/html /var/www/html.bak
+sudo unzip -o /var/www/html.zip -d /var/www
+
+# Step 3: Restore the database
+#echo "Restoring database from $DB_FILE..."
+sudo mysql dbresume90 < /var/www/dbresume90_14-24.sql
+
+# Fetch the new IP address
+ NEW_URL=$(curl -s http://checkip.amazonaws.com)
+ 
+ # Fetch the old URL from the database
+OldURL=$(sudo mysql $DBName -Ns -e "SELECT CONCAT('http://', SUBSTRING_INDEX(SUBSTRING_INDEX(option_value, '/', 3), '://', -1)) AS OldURL FROM wp_options WHERE option_name = 'siteurl' OR option_name = 'home' LIMIT 1;")
+
+
+# Update URLs in the database
+#echo "Updating WordPress database URLs from $OldURL to $NEW_URL..."
+
+mysql -u $DBUser -p$DBPassword -h $RDS_ENDPOINT $DBName <<EOF
+UPDATE wp_options SET option_value = replace(option_value, '$OldURL', 'http://$NEW_URL') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE wp_posts SET guid = replace(guid, '$OldURL','http://$NEW_URL');
+UPDATE wp_posts SET post_content = replace(post_content, '$OldURL', 'http://$NEW_URL');
+UPDATE wp_postmeta SET meta_value = replace(meta_value,'$OldURL','http://$NEW_URL');
+EOF
+
+echo "WordPress database URLs updated successfully."
+# change owner of wordpress directory
+sudo chown -R apache:apache /var/www/html
+
 sudo systemctl restart httpd
 sudo systemctl start mariadb
 
