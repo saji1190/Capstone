9b9112d1ada0f48f69a3b32ce5d507ba29c38cb0
saji1190 <sajithomas1190@gmail.com>
2024-07-23 16:31:10 +0200

    Adding allow rule for RDS from EC2

--- sg.tf
+++ sg.tf
@@ -33,3 +33,31 @@ resource "aws_security_group" "wordpress_sg" {
     Name = "wordpress_sg ${var.tagNameDate}"
   }
 }
+
+# Create Security group for RDS Mysql Database
+resource "aws_security_group" "rds-sg" {
+  name        = "mysql_sg"
+  description = "Security group for MySQL database"
+
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description     = "MySQL"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.wordpress_sg.id]
+  }
+
+  egress {
+    description = "All traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.cidr_blocks
+  }
+
+  tags = {
+    Name = "rds_sg ${var.tagNameDate}"
+  }
+}
